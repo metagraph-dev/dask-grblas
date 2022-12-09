ebae372332dfe8a302988ef731d5fabe3b3f5106
Erik Welch <erik.n.welch@gmail.com>
2022-12-09 03:09:08 +0100

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     packages=["dask_grblas"],
     license="Apache License 2.0",
     setup_requires=[],
-    install_requires=["grblas >= 1.3.15", "dask[array]"],
+    install_requires=["python-graphblas >= 2022.11.0", "dask[array]"],
     tests_require=["pytest"],
 )
